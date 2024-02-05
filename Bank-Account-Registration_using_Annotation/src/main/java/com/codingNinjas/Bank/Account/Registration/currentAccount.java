556f4a5426d2a7030f039cf057965551e872fe4e
Nityasundar Mondal <103302347+nityasundar2743@users.noreply.github.com>
2024-02-05 22:26:42 +0530

    Add files via upload

---
+++ Bank-Account-Registration_using_Annotation/src/main/java/com/codingNinjas/Bank/Account/Registration/currentAccount.java
@@ -0,0 +1,48 @@
+package com.codingNinjas.Bank.Account.Registration;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+/**
+
+  This class is an implementation of a "Account" Interface based on the selection 
+  done in the console for account type .You need to complete this class
+  based on the following tasks.
+
+     Tasks:
+   a. Create attribute amount(double) 
+   b. Override the methods of Account Interface.
+   c. Build the logic for all the methods based on the description mentioned in the Account Interface.
+
+**/
+@Component("currentAccount")
+@Scope("prototype")
+public class currentAccount implements Account{
+	double amount;
+
+	@Override
+	public String getAccountType() {
+		// TODO Auto-generated method stub
+		return "Current Account";
+	}
+	@PostConstruct
+	public void init() {
+		System.out.println("Current Account has been created and i'm the init() method");
+	}
+
+	@Override
+	public void addBalance(double balance) {
+		// TODO Auto-generated method stub
+		this.amount+=balance;
+	}
+
+	@Override
+	public double getBalance() {
+		// TODO Auto-generated method stub
+		return amount;
+	}
+	
+	
+}
